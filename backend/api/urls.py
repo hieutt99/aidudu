029c34750452284625c9ee1b4c1a910aaf51476b
minhdq99hp <minhdq99hp@gmail.com>
2021-11-05 23:04:46 +0700

    update

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,6 +1,15 @@
 from django.urls import path
 from .views import *
 
+
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register(r'workspaces', WorkspaceViewSet, basename='workspace')
+
 urlpatterns = [
     path('me', CurrentUserAPIView.as_view()),
 ]
+
+urlpatterns += router.urls
+
--- backend/api/views.py
+++ backend/api/views.py
@@ -5,8 +5,14 @@ from rest_framework.views import APIView
 from django.contrib.auth import get_user_model
 from api.serializers import *
 from rest_framework.parsers import FormParser, MultiPartParser, JSONParser
+from rest_framework.viewsets import ModelViewSet
+from api.models import *
 
 class CurrentUserAPIView(APIView):
     def get(self, request):
         serializer = UserSerializer(request.user)
         return Response(serializer.data)
+
+
+class WorkspaceViewSet(ModelViewSet):
+    model = Workspace
