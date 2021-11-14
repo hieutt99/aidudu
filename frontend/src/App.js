56a02bda9321f102910c5f5c27e3ab9bb2603b2c
lehoangtran289 <lehoangtran289@gmail.com>
2021-11-14 22:59:14 +0700

    init metronic theme

--- frontend/src/App.js
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div className="App">
-      Helloworld
-    </div>
-  );
-}
-
-export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,16 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+/**
+ * Create React App entry point. This and `public/index.html` files can not be
+ * changed or moved.
+ */
+import "react-app-polyfill/ie11";
+import "react-app-polyfill/stable";
+import React from "react";
+import ReactDOM from "react-dom";
+import axios from "axios";
+import * as _redux from "./redux";
+import store, { persistor } from "./redux/store";
+import App from "./app/App";
+import "./index.scss"; // Standard version
+// import "./sass/style.react.rtl.css"; // RTL version
+import "./_metronic/_assets/plugins/keenthemes-icons/font/ki.css";
+import "socicon/css/socicon.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "./_metronic/_assets/plugins/flaticon/flaticon.css";
+import "./_metronic/_assets/plugins/flaticon2/flaticon.css";
+// Datepicker
+import "react-datepicker/dist/react-datepicker.css";
+import {
+  MetronicLayoutProvider,
+  MetronicSplashScreenProvider,
+  MetronicSubheaderProvider
+} from "./_metronic/layout";
+import {MetronicI18nProvider} from "./_metronic/i18n";
+
+/**
+ * Base URL of the website.
+ *
+ * @see https://facebook.github.io/create-react-app/docs/using-the-public-folder
+ */
+const { PUBLIC_URL } = process.env;
+
+/**
+ * Creates `axios-mock-adapter` instance for provided `axios` instance, add
+ * basic Metronic mocks and returns it.
+ *
+ * @see https://github.com/ctimmerm/axios-mock-adapter
+ */
+/* const mock = */ _redux.mockAxios(axios);
+
+/**
+ * Inject metronic interceptors for axios.
+ *
+ * @see https://github.com/axios/axios#interceptors
+ */
+_redux.setupAxios(axios, store);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <MetronicI18nProvider>
+    <MetronicLayoutProvider>
+      <MetronicSubheaderProvider>
+        <MetronicSplashScreenProvider>
+          <App store={store} persistor={persistor} basename={PUBLIC_URL} />
+        </MetronicSplashScreenProvider>
+      </MetronicSubheaderProvider>
+    </MetronicLayoutProvider>
+  </MetronicI18nProvider>,
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
