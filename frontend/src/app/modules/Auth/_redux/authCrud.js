70cb991806bfd79150bbd1acacbf905518eab329
DukeLee <DungLe30@bitbucket.org>
2021-11-16 23:05:41 +0700

    refactor login

--- frontend/src/app/modules/Auth/_redux/authCrud.js
+++ frontend/src/app/modules/Auth/_redux/authCrud.js
@@ -1,24 +1,34 @@
 import axios from "axios";
+import { BACKEND_URL } from "../../../../../config";
+export const LOGIN_URL = BACKEND_URL+"auth/login";
+export const REGISTER_URL = BACKEND_URL+"auth/register";
+export const REQUEST_PASSWORD_URL = BACKEND_URL+"auth/forgot-password";
 
-export const LOGIN_URL = "api/auth/login";
-export const REGISTER_URL = "api/auth/register";
-export const REQUEST_PASSWORD_URL = "api/auth/forgot-password";
-
-export const ME_URL = "api/me";
+export const ME_URL = BACKEND_URL+"me";
 
 export function login(email, password) {
-  return axios.post(LOGIN_URL, { email, password });
-}
-
-export function register(email, fullname, username, password) {
-  return axios.post(REGISTER_URL, { email, fullname, username, password });
-}
-
-export function requestPassword(email) {
-  return axios.post(REQUEST_PASSWORD_URL, { email });
-}
-
-export function getUserByToken() {
-  // Authorization head should be fulfilled in interceptor.
-  return axios.get(ME_URL);
-}
+    const username = email;
+    return axios.post(LOGIN_URL, { username, password });
+  }
+  
+  export function register(email, fullname, username, password) {
+    let splitted = fullname.split(" ");
+    
+    let first_name = splitted[0];
+  
+    let last_name = '';
+    if(splitted.length > 1){
+      last_name = splitted.slice(1).join(" ").trim();
+    }
+    return axios.post(REGISTER_URL, { email, first_name, last_name, username, password });
+  }
+  
+  export function requestPassword(email) {
+    return axios.post(REQUEST_PASSWORD_URL, { email });
+  }
+  
+  export function getUserByToken() {
+    // Authorization head should be fulfilled in interceptor.
+  
+    return axios.get(ME_URL);
+  }
--- frontend/src/app/modules/Auth/pages/Login.js
+++ frontend/src/app/modules/Auth/pages/Login.js
@@ -112,12 +112,13 @@ function Login(props) {
             <div className="alert-text font-weight-bold">{formik.status}</div>
           </div>
         ) : (
-          <div className="mb-10 alert alert-custom alert-light-info alert-dismissible">
-            <div className="alert-text ">
-              Use account <strong>admin@demo.com</strong> and password{" "}
-              <strong>demo</strong> to continue.
-            </div>
-          </div>
+            <></>
+        //   <div className="mb-10 alert alert-custom alert-light-info alert-dismissible">
+        //     <div className="alert-text ">
+        //       Use account <strong>admin@demo.com</strong> and password{" "}
+        //       <strong>demo</strong> to continue.
+        //     </div>
+        //   </div>
         )}
 
         <div className="form-group fv-plugins-icon-container">
