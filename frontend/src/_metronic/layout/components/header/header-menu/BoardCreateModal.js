0c61bd806ffa61eb32e353feca4ad324495bdf2c
hoangtl1 <hoangtl1@viettel.com.vn>
2021-12-10 17:57:11 +0700

    board modal POST done

--- frontend/src/_metronic/layout/components/header/header-menu/BoardCreateModal.js
+++ frontend/src/_metronic/layout/components/header/header-menu/BoardCreateModal.js
@@ -4,24 +4,48 @@ import { useLocation } from 'react-router-dom';
 import * as Yup from 'yup';
 import { useSelector } from 'react-redux';
 import { Formik } from 'formik';
+import axios from 'axios';
+import { BACKEND_ORIGIN } from '../../../../../config';
 
+export const POST_CREATE_BOARD_URL = BACKEND_ORIGIN + 'api/v1/boards/';
+
+// TODO: add success toast
 function BoardCreateModal({ openBoard, setOpenBoard, workspaces }) {
   const location = useLocation();
   const user = useSelector((state) => state.auth.user);
 
   const [loading, setLoading] = useState(false);
-  const [data, setData] = useState({});
 
   useEffect(() => {
-    console.log("workspaces: " + workspaces);
-    console.log("create board data: " + data);
-  }, [workspaces, data]);
+    console.log('workspaces: ');//fixme
+    console.log(workspaces);
+  }, [workspaces]);
 
   const handleBoardSubmit = (values) => {
-    setData({
-      ...data,
-      ...values
-    });
+    console.log(values);//fixme
+
+    let formData = new FormData();
+    formData.append('background', values.background ? values.background : '');
+    formData.append('name', values.title);
+    formData.append('workspace', values.workspace);
+
+    console.log("formData");
+    console.log(formData);
+
+    setLoading(true);
+    axios.post(POST_CREATE_BOARD_URL, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+      .then((response) => {
+        console.log(response); //fixme
+        setLoading(false);
+        handleBoardClose();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const handleBoardClose = () => {
@@ -30,7 +54,7 @@ function BoardCreateModal({ openBoard, setOpenBoard, workspaces }) {
 
   const schema = Yup.object().shape({
     title: Yup.string().required(),
-    background: Yup.string(),
+    background: Yup.mixed(),
     workspace: Yup.string()
   });
 
@@ -45,8 +69,8 @@ function BoardCreateModal({ openBoard, setOpenBoard, workspaces }) {
           onSubmit={handleBoardSubmit}
           initialValues={{
             title: '',
-            background: '',
-            workspace: workspaces.length > 0 ? workspaces[0].name : ''
+            background: null,
+            workspace: workspaces.length > 0 ? workspaces[0].id : ''
           }}
         >
           {
@@ -57,20 +81,21 @@ function BoardCreateModal({ openBoard, setOpenBoard, workspaces }) {
                values,
                touched,
                isValid,
-               errors
+               errors,
+               setFieldValue
              }) => (
               <Form noValidate onSubmit={handleSubmit}>
                 <Form.Group>
                   <Form.Label>Title</Form.Label>
                   <Form.Control
-                    type="text"
-                    placeholder="Board title"
-                    name="title"
+                    type='text'
+                    placeholder='Board title'
+                    name='title'
                     value={values.title}
                     onChange={handleChange}
                     isInvalid={!!errors.title}
                   />
-                  <Form.Control.Feedback type="invalid">
+                  <Form.Control.Feedback type='invalid'>
                     {errors.title}
                   </Form.Control.Feedback>
                 </Form.Group>
@@ -79,27 +104,33 @@ function BoardCreateModal({ openBoard, setOpenBoard, workspaces }) {
                   <Form.Control
                     type={'file'}
                     name={'background'}
-                    value={values.background}
-                    onChange={handleChange}
+                    // value={values.background}
+                    onChange={(e) => {
+                      setFieldValue('background', e.currentTarget.files[0]);
+                    }}
                   />
                 </Form.Group>
                 <Form.Group>
                   <Form.Label>Choose workspace</Form.Label>
                   <Form.Control
-                    as="select"
-                    name="workspace"
+                    as='select'
+                    name='workspace'
                     onBlur={handleBlur}
                     value={values.workspace}
                     onChange={handleChange}
                   >
                     {
                       workspaces.map((w, key) => (
-                        <option key={key} value={w.name}>{w.name}</option>
+                        <option key={key} value={w.id}>{w.name}</option>
                       ))
                     }
                   </Form.Control>
                 </Form.Group>
-                <Button type='submit'>Submit</Button>
+                <Button type='submit' className={'mr-5'}>
+                  <span>Submit</span>
+                  {loading && <span className='ml-2 spinner spinner-white'/>}
+                </Button>
+                <Button variant={'light'} onClick={handleBoardClose}>Cancel</Button>
               </Form>
             )}
         </Formik>
