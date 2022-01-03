21cf4738c8ea3568759772e1b30068242cc5b186
tran-haison <haison250199@gmail.com>
2022-01-03 18:33:16 +0700

    Refactor Board page and create board member dialog

--- frontend/src/app/modules/Dashboard/pages/board/components/Board.js
+++ frontend/src/app/modules/Dashboard/pages/board/components/Board.js
@@ -2,17 +2,18 @@ import React, { useState, useRef, useEffect } from 'react';
 import { DragDropContext } from 'react-beautiful-dnd';
 import { Button, Container, Popover, Overlay } from 'react-bootstrap';
 import { FiStar } from "react-icons/fi";
-import { BsPeople, BsPersonPlus, BsTags, BsInboxes } from "react-icons/bs";
-import { BiArrowToLeft, BiWorld } from "react-icons/bi";
+import { BsPersonPlus, BsTags, BsInboxes } from "react-icons/bs";
+import { BiArrowToLeft } from "react-icons/bi";
 import { AiOutlinePlus } from "react-icons/ai";
-import { MdClose, MdOutlineDashboard, MdLockOutline, MdCorporateFare } from "react-icons/md";
-import { GrGroup } from "react-icons/gr";
+import { MdClose, MdOutlineDashboard } from "react-icons/md";
 import BoardList from './board-list/BoardList';
 import axios from 'axios';
 import {
   lightGreyColor, lightGreyBackground, iconSize24, iconSize20, iconSize34,
   contentBackgroundMask, menuContainer, menuDescription, listContainer, backgroundAddNewList, popoverDialogContainer
 } from './BoardStyles';
+import BoardWorkspaceVisibility from './board-workspace-visibility/BoardWorkspaceVisibility';
+import BoardMember from './board-member/BoardMember';
 
 // APIs
 export const BASE_URL = 'http://127.0.0.1:8000';
@@ -189,17 +190,6 @@ function Board(props) {
     }
   };
 
-  // Open/close dialog workspace visibility
-  const dialogWorkspaceVisibilityTarget = useRef(null);
-  const [isDialogWorkspaceVisibilityOpen, setDialogWorkspaceVisibility] = useState(false);
-  const onWorkspaceVisibleButtonClicked = () => {
-    if (isDialogWorkspaceVisibilityOpen) {
-      setDialogWorkspaceVisibility(false);
-    } else {
-      setDialogWorkspaceVisibility(true);
-    }
-  };
-
   return (
     <Container fluid className='p-0 m-0'>
       <DragDropContext onDragEnd={onDragEnd}>
@@ -217,23 +207,19 @@ function Board(props) {
                 <FiStar style={iconSize20} />
               </div>
 
-              {/* Workspace visible */}
-              <div
-                className={"btn mx-2 p-3 rounded d-flex align-items-center"} style={lightGreyBackground}
-                ref={dialogWorkspaceVisibilityTarget} onClick={onWorkspaceVisibleButtonClicked}
-              >
-                <BsPeople style={iconSize20} />
-                <h6 className='my-0 mx-2'>Workspace Visible</h6>
-              </div>
+              {/* Workspace visibility */}
+              <BoardWorkspaceVisibility />
 
               {/* List of members */}
-              <div className={"mx-2 p-2 d-flex align-items-center"}>
-                {/* {
-                  board["members"].map((member, index) => {
-                    return <img className={"rounded-circle bg-success"} style={iconSize24}/>
-                  })
-                } */}
-              </div>
+              {board["members"] !== undefined && board["members"].length > 0 &&
+                <div className={"mx-2 p-2 d-flex align-items-center"}>
+                  {
+                    board["members"].map((member, i) => {
+                      return <BoardMember key={member.id} member={member}/>
+                    })
+                  }
+                </div>
+              }
 
               {/* Button invite member */}
               <Button
@@ -401,74 +387,6 @@ function Board(props) {
                   <Button variant='primary' className='mx-3 mb-3 mt-0'>
                     Send invitation
                   </Button>
-
-                </div>
-              </Popover>
-            )}
-          </Overlay>
-
-          {/* Dialog workspace visibility */}
-          <Overlay target={dialogWorkspaceVisibilityTarget.current} show={isDialogWorkspaceVisibilityOpen} placement="bottom">
-            {(props) => (
-              <Popover {...props}>
-                <div className='rounded bg-white p-0 d-flex flex-column' style={popoverDialogContainer} >
-
-                  {/* Header */}
-                  <div className='d-flex justify-content-between align-items-center p-3'>
-                    <div className='btn p-0' onClick={() => { setDialogWorkspaceVisibility(false) }}>
-                      <MdClose style={iconSize20} />
-                    </div>
-                    <h6 className='m-0'>Workspace visibility</h6>
-                    <div className='btn p-0' onClick={() => { setDialogWorkspaceVisibility(false) }}>
-                      <MdClose style={iconSize20} />
-                    </div>
-                  </div>
-
-                  <hr className='m-0' />
-
-                  {/* Private */}
-                  <div className='d-flex p-3'>
-                    <div>
-                      <MdLockOutline style={iconSize20} />
-                    </div>
-                    <div className='d-flex flex-column mx-3'>
-                      <h6 className='m-0'><strong>Private</strong></h6>
-                      <p className='m-0'>Only board member can see and edit this board.</p>
-                    </div>
-                  </div>
-
-                  {/* Workspace */}
-                  <div className='d-flex p-3'>
-                    <div>
-                      <GrGroup style={iconSize20} />
-                    </div>
-                    <div className='d-flex flex-column mx-3'>
-                      <h6 className='m-0'><strong>Workspace</strong></h6>
-                      <p className='m-0'>All members of the Workspace can see and edit this board. The board must be added to a Workspace to enable this.</p>
-                    </div>
-                  </div>
-
-                  {/* Organization */}
-                  <div className='d-flex p-3'>
-                    <div>
-                      <MdCorporateFare style={iconSize20} />
-                    </div>
-                    <div className='d-flex flex-column mx-3'>
-                      <h6 className='m-0'><strong>Organization</strong></h6>
-                      <p className='m-0'>All members of the organization can see this board. The board must be added to an enterprise Workspace to enable this.</p>
-                    </div>
-                  </div>
-
-                  {/* Public */}
-                  <div className='d-flex p-3'>
-                    <div>
-                      <BiWorld style={iconSize20} />
-                    </div>
-                    <div className='d-flex flex-column mx-3'>
-                      <h6 className='m-0'><strong>Public</strong></h6>
-                      <p className='m-0'>Anyone on the internet can see this board. Only board members can edit.</p>
-                    </div>
-                  </div>
 
                 </div>
               </Popover>
---
+++ frontend/src/app/modules/Dashboard/pages/board/components/board-member/BoardMember.js
@@ -0,0 +1,76 @@
+import React, { useState, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { Button, Popover, Overlay, Image } from 'react-bootstrap';
+import { MdClose } from "react-icons/md";
+import axios from 'axios';
+import { iconSize24, iconSize20, iconSize50, popoverDialogContainer } from '../BoardStyles';
+
+const BoardMember = (props) => {
+
+    const member = props.member;
+    const user = useSelector((state) => state.auth.user);
+
+    // Open/close dialog board member 
+    const dialogMemberTarget = useRef(null);
+    const [isDialogMemberOpen, setDialogMember] = useState(false);
+    const onMemberClicked = () => {
+        if (isDialogMemberOpen) {
+            setDialogMember(false);
+        } else {
+            setDialogMember(true);
+        }
+    };
+
+    return (
+        <>
+            {/* Member's avatar */}
+            <img
+                className={"rounded-circle"} style={iconSize24} src={member.avatar} type='button'
+                data-toggle="tooltip" data-placement="bottom" title={member.username}
+                ref={dialogMemberTarget} onClick={onMemberClicked}
+            />
+
+            {/* Dialog board member */}
+            <Overlay target={dialogMemberTarget.current} show={isDialogMemberOpen} placement="bottom">
+                {(props) => (
+                    <Popover {...props}>
+                        <div className='rounded bg-white p-0 d-flex flex-column' style={popoverDialogContainer} >
+
+                            {/* Header */}
+                            <div className='d-flex justify-content-between align-items-center p-3'>
+                                <div className='btn p-0' onClick={() => { setDialogMember() }}>
+                                    <MdClose style={iconSize20} />
+                                </div>
+                                <h6 className='m-0'>{member.username}</h6>
+                                <div className='btn p-0' onClick={() => { setDialogMember() }}>
+                                    <MdClose style={iconSize20} />
+                                </div>
+                            </div>
+
+                            <hr className='m-0' />
+
+                            {/* User's info */}
+                            <div className='d-flex p-3 align-items-center'>
+                                <div>
+                                    <Image roundedCircle style={iconSize50} src={member.avatar} />
+                                </div>
+                                <div className='d-flex flex-column mx-3'>
+                                    <h6 className='m-0'><strong>{member.username}</strong></h6>
+                                </div>
+                            </div>
+
+                            {/* Current user can leave board and remove others from board */}
+                            {member.id === user.id
+                                ? <Button variant='danger' className='mx-3 mt-0 mb-3 py-2'>Leave board</Button>
+                                : <Button variant='danger' className='mx-3 mt-0 mb-3 py-2'>Remove from board...</Button>
+                            }
+
+                        </div>
+                    </Popover>
+                )}
+            </Overlay>
+        </>
+    )
+};
+
+export default BoardMember;
