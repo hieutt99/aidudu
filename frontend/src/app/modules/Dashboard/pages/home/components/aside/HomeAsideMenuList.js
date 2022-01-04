1d9eaec8794d4a41566d78a3b28798359f31cc8e
DukeLee <DungLe30@bitbucket.org>
2022-01-04 20:33:52 +0700

    fix_sidebar_ux_again

--- frontend/src/app/modules/Dashboard/pages/home/components/aside/HomeAsideMenuList.js
+++ frontend/src/app/modules/Dashboard/pages/home/components/aside/HomeAsideMenuList.js
@@ -4,6 +4,7 @@ import { NavLink } from "react-router-dom";
 import { Dropdown } from "react-bootstrap";
 import SVG from "react-inlinesvg";
 import { toAbsoluteUrl, checkIsActive } from "../../../../../../../_metronic/_helpers";
+import { WorkspaceMenuItem } from "./WorkspaceMenuItem";
 
 export function HomeAsideMenuList({ layoutProps, workspaces, handleWorkspaceModalOpen }) {
     const location = useLocation();
@@ -56,59 +57,7 @@ export function HomeAsideMenuList({ layoutProps, workspaces, handleWorkspaceModa
         </li>
         {workspaces_array.map((workspace, key) =>
         <>
-        <li
-                className={clicked===true ? `menu-item menu-item-submenu menu-item-open`: `menu-item menu-item-submenu`}
-                aria-haspopup="true"
-                data-menu-toggle="hover"
-                key={key}
-                onClick={handleClicked}
-            >
-                <div className="menu-link menu-toggle ">
-                    <span className="svg-icon menu-icon">
-                        <SVG src={toAbsoluteUrl("/media/svg/icons/Home/Library.svg")} />
-                    </span>
-                    <span className="menu-text text-black">{workspace.name}</span>
-                    <i className="menu-arrow" />
-                </div>
-                <div className="menu-submenu ">
-                    <i className="menu-arrow" />
-                    <ul className="menu-subnav">
-                        <li
-                            className={`menu-item`}
-                            aria-haspopup="true"
-                        >
-                            <NavLink className="menu-link" to={`/workspaces/${workspace.id}`}>
-                                <span className="svg-icon menu-icon">
-                                    <SVG src={toAbsoluteUrl("/media/svg/icons/Home/Library.svg")} />
-                                </span>
-                                <span className="menu-text text-black">Boards</span>
-                            </NavLink>
-                        </li>
-                        <li
-                            className={`menu-item `}
-                            aria-haspopup="true"
-                        >
-                            <NavLink className="menu-link" to={`/workspaces/${workspace.id}/members`}>
-                            <span className="svg-icon menu-icon">
-                                <SVG src={toAbsoluteUrl("/media/svg/icons/General/User.svg")} />
-                            </span>
-                            <span className="menu-text text-black">Members</span>
-                            </NavLink>
-                        </li>
-                        <li
-                            className={`menu-item `}
-                            aria-haspopup="true"
-                        >
-                            <NavLink className="menu-link" to={`/workspaces/${workspace.id}/settings`}>
-                            <span className="svg-icon menu-icon">
-                                <SVG src={toAbsoluteUrl("/media/svg/icons/Tools/Tools.svg")} />
-                            </span>
-                            <span className="menu-text text-black">Settings</span>
-                            </NavLink>
-                        </li>
-                    </ul>
-                </div>
-            </li>
+            <WorkspaceMenuItem workspace={workspace} />
         </>
            
 
---
+++ frontend/src/app/modules/Dashboard/pages/home/components/aside/WorkspaceMenuItem.js
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { NavLink } from "react-router-dom";
+import { Dropdown } from "react-bootstrap";
+import SVG from "react-inlinesvg";
+import { toAbsoluteUrl, checkIsActive } from "../../../../../../../_metronic/_helpers";
+
+export function WorkspaceMenuItem({workspace}) {
+    // const workspace=workspace
+    console.log(workspace)
+    const [clicked, setClicked] = useState(false)
+    function handleClicked(){
+        setClicked(!clicked)
+    }
+    return (
+        <li
+                className={clicked===true ? `menu-item menu-item-submenu menu-item-open`: `menu-item menu-item-submenu`}
+                aria-haspopup="true"
+                data-menu-toggle="hover"
+                key={workspace.id}
+                onClick={handleClicked}
+            >
+                <div className="menu-link menu-toggle ">
+                    <span className="svg-icon menu-icon">
+                        <SVG src={toAbsoluteUrl("/media/svg/icons/Home/Library.svg")} />
+                    </span>
+                    <span className="menu-text text-black">{workspace.name}</span>
+                    <i className="menu-arrow" />
+                </div>
+                <div className="menu-submenu ">
+                    <i className="menu-arrow" />
+                    <ul className="menu-subnav">
+                        <li
+                            className={`menu-item`}
+                            aria-haspopup="true"
+                        >
+                            <NavLink className="menu-link" to={`/workspaces/${workspace.id}`}>
+                                <span className="svg-icon menu-icon">
+                                    <SVG src={toAbsoluteUrl("/media/svg/icons/Home/Library.svg")} />
+                                </span>
+                                <span className="menu-text text-black">Boards</span>
+                            </NavLink>
+                        </li>
+                        <li
+                            className={`menu-item `}
+                            aria-haspopup="true"
+                        >
+                            <NavLink className="menu-link" to={`/workspaces/${workspace.id}/members`}>
+                            <span className="svg-icon menu-icon">
+                                <SVG src={toAbsoluteUrl("/media/svg/icons/General/User.svg")} />
+                            </span>
+                            <span className="menu-text text-black">Members</span>
+                            </NavLink>
+                        </li>
+                        <li
+                            className={`menu-item `}
+                            aria-haspopup="true"
+                        >
+                            <NavLink className="menu-link" to={`/workspaces/${workspace.id}/settings`}>
+                            <span className="svg-icon menu-icon">
+                                <SVG src={toAbsoluteUrl("/media/svg/icons/Tools/Tools.svg")} />
+                            </span>
+                            <span className="menu-text text-black">Settings</span>
+                            </NavLink>
+                        </li>
+                    </ul>
+                </div>
+            </li>
+    )
+}
